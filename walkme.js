f7cee8ad848020a7b69fe4f7b51028f2d0121fe3
Omer Dobzinsky <omer.d@walkme.com>
2020-04-23 15:03:05 +0300

    implement multiple positions using strategy

--- walkme.js
+++ walkme.js
@@ -6,27 +6,51 @@
         text: 'drag me'
     };
 
-    function drawBalloon(data) {
-        const target = document.getElementById(data.targetId);
-        const balloon = document.createElement('div');
-        
-        const targetRect = target.getClientRects()[0];
-        
-        balloon.style.position = 'absolute';
+    function drawBellow(data, targetRect, balloon) {
         balloon.style.top = `${targetRect.bottom + 5}px`;
         balloon.style.left = `${(targetRect.right + targetRect.left - data.width) / 2}px`;
+        balloon.style.borderTop = '3px solid red';
+    }
+
+    function drawAbove(data, targetRect, balloon) {
+        balloon.style.top = `${targetRect.top - data.height - 5}px`;
+        balloon.style.left = `${(targetRect.right + targetRect.left - data.width) / 2}px`;
+        balloon.style.borderBottom = '3px solid red';
+    }
+
+    function drawLeft(data, targetRect, balloon) {
+        balloon.style.top = `${(targetRect.top + targetRect.bottom - data.height) / 2}px`;
+        balloon.style.left = `${targetRect.left - data.width - 5}px`;
+        balloon.style.borderRight = '3px solid red';
+    }
+
+    function drawRight(data, targetRect, balloon) {
+        balloon.style.top = `${(targetRect.top + targetRect.bottom - data.height) / 2}px`;
+        balloon.style.left = `${targetRect.right + 5}px`;
+        balloon.style.borderLeft = '3px solid red';
+    }
+
+    function drawBalloon(data, drawStrategy = drawBellow) {
+        const target = document.getElementById(data.targetId);
+        const balloon = document.createElement('div');
+
+        const targetRect = target.getClientRects()[0];
+
+        drawStrategy(data, targetRect, balloon);
+
+        balloon.style.position = 'absolute';
         balloon.style.width = `${data.width}px`;
         balloon.style.height = `${data.height}px`;
         balloon.style.backgroundColor = 'coral';
-        balloon.style.borderTop = '3px solid red';
         balloon.innerText = data.text;
-        
+
         target.parentElement.appendChild(balloon);
     }
 
     window.walkme = {
-        play() {
-            drawBalloon(balloonData);
-        }
+        play(drawStrategy) {
+            drawBalloon(balloonData, drawStrategy);
+        },
+        drawStrategies: { drawBellow, drawAbove, drawLeft, drawRight }
     };
 })();
